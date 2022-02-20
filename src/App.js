8680bfe347bb5633a3f59907f789acf6e80228c5
Gutu Galuppo <galuppodev@gmail.com>
2022-02-20 22:12:41 +0100

    feat: add controls functions, import env vars, passing props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Player from './components/Player';
+import Playlist from './components/Playlist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [songs, setSongs] = useState([]);
+	const [currentSongIndex, setCurrentSongIndex] = useState(0);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	useEffect(() => {
+		const BASE_URL = process.env.REACT_APP_API_URL
+		const url = `${BASE_URL}/song/trending`
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				setSongs(data)
+				setIsLoading(false)
+			})
+	}, [])
+
+	const nextTrack = () => {
+		let currSong = currentSongIndex;
+		currSong++;
+		if (currSong >= songs.length) {
+			currSong = 0;
+		}
+		playTrackByIndex(currSong)
+	}
+
+	const prevTrack = () => {
+		let currSong = currentSongIndex;
+		currSong--;
+		if (currSong < 0) {
+			currSong = songs.length - 1;
+		}
+		playTrackByIndex(currSong)
+	}
+
+	const playTrackByIndex = (index) => {
+		setCurrentSongIndex(index);
+		setIsPlaying(true);
+	}
+
+	const pauseTrackByIndex = (index) => {
+		setCurrentSongIndex(index);
+		setIsPlaying(false);
+	}
+
+	const togglePlay = () => {
+		setIsPlaying(!isPlaying);
+	}
+
+	if (isLoading) return <div>Loading...</div>
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				AudioPlayer
+			</header>
+
+			<Player
+				songs={songs}
+				currentSongIndex={currentSongIndex}
+				isPlaying={isPlaying}
+				togglePlay={togglePlay}
+				nextTrack={nextTrack}
+				prevTrack={prevTrack}
+				playTrackByIndex={playTrackByIndex}
+			/>
+
+			<Playlist
+				songs={songs}
+				playTrackByIndex={playTrackByIndex}
+				pauseTrackByIndex={pauseTrackByIndex}
+				isPlaying={isPlaying}
+				currentSongIndex={currentSongIndex}
+			/>
+
+		</div>
+	);
 }
 
 export default App;
