88cd91faca1d70377ce7084d080049d5aee18af4
Gutu Galuppo <galuppodev@gmail.com>
2022-02-21 09:53:43 +0100

    refactor: cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Player from './components/Player';
 import Playlist from './components/Playlist';
